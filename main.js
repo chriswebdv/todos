d104a19da54892f6fa6f732c54f3bd5c0cdaaaab
chriswebdv <chriswebdv@gmail.com>
2024-06-05 09:28:14 -0600

    Create a function in JS to add the todos

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 const textarea = document.querySelector("textarea");
 const addButton = document.querySelector(".addButton");
 const todoContainer = document.querySelector(".todo-container");
+
+let todoList = [];
+
+function addTodo() {
+  const todo = textarea.value;
+  if (!todo) {
+    return;
+  }
+  console.log("Add todo: ", todo);
+  todoList.push(todo);
+  textarea.value = ""; // resets to empty
+  updateUI();
+}
+
+function updateUI() {}
+
+addButton.addEventListener("click", addTodo);
